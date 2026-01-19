e333a422b2e136e18f9dd3758d6f5d222e9506b3
KH.Bidyut <khbidyut31@gmail.com>
2026-01-19 22:14:55 +0600

    learn how to create multipul app using app.mountpath

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 // with express
 const express = require('express')
 
-const app = express()
+const app = express() // the main app
+const admin = express() // the sub app
 
 // app.use(express.json())
 // app.use(express.raw())
 // app.use(express.text())
 // app.use(express.urlencoded())
-app.use(express.static(`${__dirname}/image`))
+// app.use(express.static(`${__dirname}/image`))
+
+admin.get('/dashboard',(req,res)=>{
+    res.send('This is the admin dashboard')
+})
 
 app.get('/',(req,res)=>{
-    res.send('Hello world')
+    res.send('Main App')
 })
 
-app.post('/', (req, res)=>{
-    console.log(req.body);
-    res.send('This is post route');
-    
-})
+app.use('/admin', admin)
+
 app.listen(3000, ()=>{
     console.log("server is runing on port 3000");
     
