7dd93d88d2b4a3e75a30f52e2df6d65cc7a272e7
KH.Bidyut <khbidyut31@gmail.com>
2026-01-19 21:48:47 +0600

    now give a plain text result becous i write app.use(express.text())

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const express = require('express')
 const app = express()
 
 // app.use(express.json())
-app.use(express.raw())
+// app.use(express.raw())
+app.use(express.text())
 
 app.get('/',(req,res)=>{
     res.send('Hello world')
 })
 
 app.post('/', (req, res)=>{
-    console.log(req.body.toString());
+    console.log(req.body);
     res.send('This is post route');
     
 })
