5a545e5bd1d672de6e45c5ce2aa051af68d57296
KH.Bidyut <khbidyut31@gmail.com>
2026-01-19 21:31:41 +0600

    now i learn how to post a json text in express.js server

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send('Hello world')
 })
 
+app.post('/', (req, res)=>{
+    console.log(req.body);
+    res.send('This is post route');
+    
+})
 app.listen(3000, ()=>{
     console.log("server is runing on port 3000");
     
