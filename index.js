01fb54e23957da2f5f25854e47d804422794c4a0
KH.Bidyut <khbidyut31@gmail.com>
2026-01-19 21:21:33 +0600

    start to learn backend with express.js and create a simple server

---
+++ index.js
@@ -0,0 +1,14 @@
+// with express
+const express = require('express')
+
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello world')
+})
+
+app.listen(3000, ()=>{
+    console.log("server is runing on port 3000");
+    
+})
