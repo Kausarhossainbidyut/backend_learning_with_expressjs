be4221fa40cfe3218bf2db5581c275350746080f
KH.Bidyut <khbidyut31@gmail.com>
2026-01-22 18:11:39 +0600

    learn how work Router.all method like this publicRouter.all('*', log); first decklear log middleware

--- routerConcept/publicRouter.js
+++ routerConcept/publicRouter.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const publicRouter = express.Router();
 
+const log = (req, res, next) => {
+    console.log(`I am logging something `);
+    next();
+}
+
+publicRouter.all('*', log);
+
 publicRouter.get('/', (req, res) => {
     res.send('Public Home Page');
 }); 
