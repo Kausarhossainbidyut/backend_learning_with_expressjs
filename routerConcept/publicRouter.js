d26d72e775ab6948197d77415a7ca5a68d0f912e
KH.Bidyut <khbidyut31@gmail.com>
2026-01-22 18:40:41 +0600

    learn router.route

--- routerConcept/publicRouter.js
+++ routerConcept/publicRouter.js
@@ -3,6 +3,24 @@ const express = require('express');
 
 const publicRouter = express.Router();
 
+publicRouter.route('/user')
+    .all((req, res, next) => {
+        console.log('Middleware for /user route');
+        next();
+    })
+    .get((req, res) => {
+        res.send('Public User GET Page');
+    })
+    .post((req, res) => {
+        res.send('Public User POST Page');
+    })
+    .put((req, res) => {
+        res.send('Public User PUT Page');
+    })
+    .delete((req, res) => {
+        res.send('Public User DELETE Page');
+    });
+
 // const log = (req, res, next) => {
 //     console.log(`I am logging something `);
 //     next();
@@ -14,24 +32,24 @@ const publicRouter = express.Router();
 //     next();
 // });
 
-publicRouter.param((param, option) => (req, res, next, value) => {
-    // Custom logic based on param and option
-    if(value === option){
-        next();
-    }else{
-        res.sendStatus(403); // Forbidden
-    }
-    next();
-});
+// publicRouter.param((param, option) => (req, res, next, value) => {
+//     // Custom logic based on param and option
+//     if(value === option){
+//         next();
+//     }else{
+//         res.sendStatus(403); // Forbidden
+//     }
+//     next();
+// });
 
-publicRouter.param('user', '12'); // middleware for :user param if user equals 12 then proceed otherwise block
+// publicRouter.param('user', '12'); // middleware for :user param if user equals 12 then proceed otherwise block
 
-publicRouter.get('/:user', (req, res) => {
-    res.send(`Hello admin`);
-}); 
+// publicRouter.get('/:user', (req, res) => {
+//     res.send(`Hello admin`);
+// }); 
 
-publicRouter.get('/about', (req, res) => {
-    res.send('Public About Page');
-});
+// publicRouter.get('/about', (req, res) => {
+//     res.send('Public About Page');
+// });
 
 module.exports = publicRouter;
